a3b83794846346d186930289d5bc1fc1975dddad
josemejias11 <jdavid07@live.com>
2025-05-06 15:09:22 -0600

    Initial working version of Task Manager app with Playwright test passing

--- server/server.js
+++ server/server.js
@@ -1,26 +1,35 @@
 const express = require('express');
-const app = express();
-const path = require('path');
 const fs = require('fs');
-const bodyParser = require('body-parser');
-const db = require('./db.json');
-app.use(bodyParser.json());
-app.use(express.static(__dirname));
-let tasks = db.tasks || [];
+const path = require('path');
+const app = express();
 
-app.get('/api/tasks', (req, res) => res.json(tasks));
-app.post('/api/tasks', (req, res) => {
-  const task = { id: Date.now(), ...req.body };
-  tasks.push(task);
-  fs.writeFileSync(path.join(__dirname, 'db.json'), JSON.stringify({ tasks }, null, 2));
-  res.status(201).json(task);
+app.use(express.json());
+app.use(express.static(__dirname + '/../'));
+
+const DB_FILE = path.join(__dirname, '../db.json');
+
+app.get('/api/tasks', (req, res) => {
+  const data = JSON.parse(fs.readFileSync(DB_FILE));
+  res.json(data.tasks || []);
 });
+
+app.post('/api/tasks', (req, res) => {
+  const data = JSON.parse(fs.readFileSync(DB_FILE));
+  const newTask = { id: Date.now(), title: req.body.title };
+  data.tasks.push(newTask);
+  fs.writeFileSync(DB_FILE, JSON.stringify(data, null, 2));
+  res.status(201).json(newTask);
+});
+
 app.delete('/api/tasks/:id', (req, res) => {
-  tasks = tasks.filter(t => t.id !== parseInt(req.params.id));
-  fs.writeFileSync(path.join(__dirname, 'db.json'), JSON.stringify({ tasks }, null, 2));
+  const data = JSON.parse(fs.readFileSync(DB_FILE));
+  data.tasks = data.tasks.filter(task => task.id !== parseInt(req.params.id));
+  fs.writeFileSync(DB_FILE, JSON.stringify(data, null, 2));
   res.status(204).end();
 });
+
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'index.html'));
+  res.sendFile(path.join(__dirname, '../index.html'));
 });
+
 app.listen(3000, () => console.log('Server running on http://localhost:3000'));
